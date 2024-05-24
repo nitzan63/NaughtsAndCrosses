5261f959d264cf12b27e95370a2566d083e8f60d
nitzanainemer <nitzan63@gmail.com>
2024-05-24 13:25:07 +0300

    Added top bar in GUI

--- naughts_and_crosses/gui.py
+++ naughts_and_crosses/gui.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import messagebox
 
 
 class NntGui:
@@ -8,8 +7,37 @@ class NntGui:
         self.master.title("Naughts and Crosses - By Nitzan")
         self.master.geometry("400x400")
         self.master.resizable(False, False)
-        label = tk.Label(self.master, text="Hello World")
-        label.pack()
+
+        self.current_turn = 1
+
+
+        # create top bar:
+        self.create_top_bar()
+
+        self.update_turn_label()
+
+    def create_top_bar(self):
+        # create and pack the frame:
+        top_frame = tk.Frame(self.master)
+        top_frame.pack(side=tk.TOP, fill=tk.X)
+
+        # place labels in the top bar:
+        self.turn_label = tk.Label(top_frame, text="", fg="green")
+        self.turn_label.pack(side=tk.LEFT)
+
+        # set the "New Game" button:
+        self.new_game_button = tk.Button(top_frame, text="New Game", command=self.reset_board)
+        self.new_game_button.pack(side=tk.RIGHT)
+
+    def reset_board(self):
+        # TODO: Implement logic
+        print("Resetting board")
+
+    def update_turn_label(self):
+        if self.current_turn == 1:
+            self.turn_label.config(text="P1 Turn O", fg="green")
+        else:
+            self.turn_label.config(text="P2 Turn X", fg="red")
 
 
 def main():
